cceee179e5f89b2dc99ce42471d639d534785210
nerjib <kabirnajib0@gmail.com>
2020-04-18 16:51:17 +0000

    get all with users

--- src/controllers/estimator.js
+++ src/controllers/estimator.js
@@ -110,7 +110,7 @@ router.get('/users/:id',Request.logRequest, async (req, res) => {
     const getAllQ = 'SELECT * FROM datatable WHERE vehicle_no= $1 and euid=$2  order by id desc';
     try {
       // const { rows } = qr.query(getAllQ);
-      const { rows } = await db.query(getAllQ, [(req.params.id).toUpperCase(), null]);
+      const { rows } = await db.query(getAllQ, [(req.params.id).toUpperCase(), '']);
       return res.status(201).send(rows);
     } catch (error) {
       if (error.routine === '_bt_check_unique') {
