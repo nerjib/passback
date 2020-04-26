124aea8644167eff20ee74954a7ffc4e92ac6eb1
nerjib <kabirnajib0@gmail.com>
2020-04-26 21:23:18 +0100

    get single passenger endpoint

--- src/controllers/estimator.js
+++ src/controllers/estimator.js
@@ -243,6 +243,22 @@ router.put('/exit/:vn', async (req, res) => {
 
 });
 
+router.get('/passengers/:id',Request.logRequest, async (req, res) => {
+
+  const getAllQ = 'SELECT * FROM passengers where pid=$1';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,[req.params.id]);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+  //  const output1 = (Estimator.covid19ImpactEstimator(req.body))
+
+});
 
 router.get('/users/:id',Request.logRequest, async (req, res) => {
 
